ecaff208498d777f5eba7fd36870ac7bf7c468c2
caroline dugan <caroline.dugan@gmail.com>
2018-07-16 14:18:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,21 @@
 class Song 
   
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres = []
+  end
+  @@count = 0
+  def count
+    count = @@count
+    return "#{@@count}"
+  end
+  def genres 
+    genres = @genre
+    return "#{@genre}"
+  end
+  
+  
